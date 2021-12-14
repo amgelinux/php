60e153e65a041a2792aeccc4ad6e962165163fea
amgelinux <angelmartinez@iescastelar.com>
2021-12-14 17:33:16 +0100

    leer fichero xml y zip

---
+++ htdocs/10_xml/ClaseJuego.php
@@ -0,0 +1,33 @@
+<?php
+class Juego {
+    private string $id;
+    private string $name;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $id
+     * @param string $name
+     */
+    public function __construct(string $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+}
+?>
